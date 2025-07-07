7dc2ba6b41ad6ec68b187b087d7b77fd6a9a0082
Yoseph <yoseph@gmail.com>
2025-07-07 06:39:16 +0200

    chore: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-    server: {
-        host: "::",
-        port: 8080,
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [
-        react()
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-}));
+  },
+});
 
